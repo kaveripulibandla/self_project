426776f09fb6561d7516f734285f6a61d4d7cf7c
kaveripulibandla <kaveri.p@kloud9.com>
2023-02-20 18:23:03 +0530

    changes

--- src/modules/Raw_layer.py
+++ src/modules/Raw_layer.py
@@ -3,5 +3,31 @@ import findspark
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
-from pyspark.sql.window import *
+import pyspark.sql.functions as F
+from pyspark.sql.functions import regexp_replace
+from pyspark.sql.window import Window
+
+
+def create_rawlayer():
+    spark = SparkSession.builder.getOrCreate()
+    df = spark.read.format("csv").option("header", True).option("inferSchema", True).load(
+        "C:\\self_project\\input_files\\sales_data.csv")
+
+    df.printSchema()
+    df.show(truncate=False)
+    df.write.mode("overwrite").format("csv").option("header", True)\
+        .save("C:\\self_project\\src\\internal_files\\raw_sales_file.csv")
+
+
+    # """"" # RAW_DATA HIVE TABLE """""
+    # df.coalesce(1).write.mode("overwrite").saveAsTable("raw_retail_details")
+    # df_log = spark.sql("select * from raw_retail_details")
+    # df_log.show()
+    #
+    # df_log = spark.sql("select count(*) from raw_retail_details")
+    # df_log.show()
+
+
+if __name__ == '__main__':
+    create_rawlayer()
 
